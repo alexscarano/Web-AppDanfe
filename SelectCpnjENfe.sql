1c475969a683fef6e636c9aa441296d20a4ff420
alexscarano <alexandre.scarano@cheffsolutions.com>
2025-01-13 12:32:53 -0300

    Primeiro commit

--- SelectCpnjENfe.sql
+++
@@ -1,8 +0,0 @@
-USE Eproc_121;
-
-
-/*SELECT Nfe.nfe FROM dbo.NotaFiscalEletronica Nfe
-WHERE Nfe.cnpjEmitente = '05494486000185'
-AND Nfe.numeroNFe = '81794';*/
-
- SELECT Nfe.cnpjEmitente, Nfe.numeroNFe, Nfe.nfe FROM dbo.NotaFiscalEletronica Nfe;
